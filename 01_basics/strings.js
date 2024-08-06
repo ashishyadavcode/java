b5cb9f00d47c767ec24aac1b83a246fa0e0d8c05
AshishYadavCodeMaster <ahirrajityadav@gmail.com>
2024-08-06 21:01:44 +0530

    strings

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -7,20 +7,21 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
 
+
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
